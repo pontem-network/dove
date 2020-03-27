694d0ff1630c4c3af923235da08f28d40447274e
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2020-03-27 16:34:05 +0300

    Cleanup according to rust-analyzer, add more handlers (#2)
    * cleanup according to rust-analyzer, add more handlers
    
    * remove filepath check for now

--- src/main_loop.rs
+++ src/main_loop.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
+use crossbeam_channel::Sender;
 use lsp_server::{Connection, Message, Notification};
+use lsp_types::notification::{
+    DidChangeTextDocument, DidCloseTextDocument, DidOpenTextDocument, PublishDiagnostics,
+};
 use lsp_types::PublishDiagnosticsParams;
-
-use lsp_types::notification::DidOpenTextDocument;
-
 use serde::de::DeserializeOwned;
+use serde::Serialize;
 
 pub fn main_loop(connection: &Connection) -> Result<()> {
     log::info!("starting example main loop");
@@ -16,40 +18,77 @@ pub fn main_loop(connection: &Connection) -> Result<()> {
                 return Ok(());
             }
         }
-        match message {
-            Message::Request(req) => {
-                log::info!("Got request: {:?}", req);
-            }
-            Message::Notification(notif) => {
-                log::info!("Got notification: {:?}", notif);
-                if let Ok(params) = notification_cast::<DidOpenTextDocument>(notif) {
-                    log::info!(
-                        "Received 'textDocument/didOpen' with text {:?}",
-                        params.text_document.text
-                    );
-
-                    let diagnostics_notif = Message::Notification(Notification::new(
-                        "textDocument/publishDiagnostics".to_string(),
-                        PublishDiagnosticsParams::new(params.text_document.uri, vec![], None),
-                    ));
-                    log::info!("Sending {:?}", &diagnostics_notif);
-                    connection.sender.send(diagnostics_notif).unwrap();
-                }
-            }
-            Message::Response(resp) => {
-                log::info!("Got response: {:?}", resp);
-            }
-        }
+        loop_turn(&connection, message)?;
     }
     Ok(())
 }
 
-pub fn notification_cast<N>(
-    notification: Notification,
-) -> std::result::Result<N::Params, Notification>
+fn loop_turn(connection: &Connection, message: Message) -> Result<()> {
+    match message {
+        Message::Request(req) => {
+            log::info!("Got request: {:?}", req);
+        }
+        Message::Notification(notif) => {
+            log::info!("Got notification: {:?}", notif);
+            on_notification(&connection.sender, notif)?;
+        }
+        Message::Response(resp) => {
+            log::info!("Got response: {:?}", resp);
+        }
+    };
+    Ok(())
+}
+
+fn on_notification(msg_sender: &Sender<Message>, notif: Notification) -> Result<()> {
+    let notif = match notification_cast::<DidOpenTextDocument>(notif) {
+        Ok(params) => {
+            let uri = params.text_document.uri;
+            let params = PublishDiagnosticsParams::new(uri, vec![], None);
+            let diag_notif = notification_new::<PublishDiagnostics>(params);
+
+            log::info!("Sending {:?}", &diag_notif);
+            msg_sender.send(diag_notif.into()).unwrap();
+            return Ok(());
+        }
+        Err(notif) => notif,
+    };
+    let notif = match notification_cast::<DidChangeTextDocument>(notif) {
+        Ok(params) => {
+            let uri = params.text_document.uri;
+            let params = PublishDiagnosticsParams::new(uri, vec![], None);
+            let diag_notif = notification_new::<PublishDiagnostics>(params);
+
+            log::info!("Sending {:?}", &diag_notif);
+            msg_sender.send(diag_notif.into()).unwrap();
+            return Ok(());
+        }
+        Err(notif) => notif,
+    };
+    let notif = match notification_cast::<DidCloseTextDocument>(notif) {
+        Ok(_) => {
+            return Ok(());
+        }
+        Err(notif) => notif,
+    };
+    if notif.method.starts_with("$/") {
+        return Ok(());
+    }
+    log::error!("unhandled notification: {:?}", notif);
+    Ok(())
+}
+
+pub fn notification_cast<N>(notification: Notification) -> Result<N::Params, Notification>
 where
     N: lsp_types::notification::Notification,
     N::Params: DeserializeOwned,
 {
     notification.extract(N::METHOD)
+}
+
+pub fn notification_new<N>(params: N::Params) -> Notification
+where
+    N: lsp_types::notification::Notification,
+    N::Params: Serialize,
+{
+    Notification::new(N::METHOD.to_string(), params)
 }
--- tests/test_server_responses.rs
+++ tests/test_server_responses.rs
@@ -1,12 +1,13 @@
+use crossbeam_channel::Receiver;
 use lsp_server::{Connection, Message, Notification, Request, RequestId};
-use lsp_types::notification::PublishDiagnostics;
+use lsp_types::notification::{DidCloseTextDocument, DidOpenTextDocument, PublishDiagnostics};
 use lsp_types::{
-    DidOpenTextDocumentParams, InitializeResult, TextDocumentItem, TextDocumentSyncCapability,
-    TextDocumentSyncKind, Url,
+    Diagnostic, DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams,
+    InitializeResult, TextDocumentIdentifier, TextDocumentItem, TextDocumentSyncCapability,
+    TextDocumentSyncKind, Url, VersionedTextDocumentIdentifier,
 };
 
-use move_language_server::main_loop::{main_loop, notification_cast};
-
+use move_language_server::main_loop::{main_loop, notification_cast, notification_new};
 use move_language_server::server::initialize_server;
 
 fn setup_test_logging() {
@@ -31,6 +32,32 @@ fn send_shutdown_requests(client_connection: &Connection) {
         serde_json::json!("null"),
     ));
     client_connection.sender.send(exit_notif).unwrap();
+}
+
+fn assert_diagnostics(
+    diagnostics_message: Message,
+    expected_document_url: Url,
+    expected_diagnostics: Vec<Diagnostic>,
+) {
+    assert!(matches!(diagnostics_message, Message::Notification(_)));
+    if let Message::Notification(notif) = diagnostics_message {
+        let params = notification_cast::<PublishDiagnostics>(notif).unwrap();
+        assert_eq!(params.uri, expected_document_url);
+        assert_eq!(params.diagnostics, expected_diagnostics);
+    }
+}
+
+fn assert_receiver_has_only_shutdown_response(client_receiver: Receiver<Message>) {
+    let shutdown_message = client_receiver.try_recv().unwrap();
+
+    assert!(matches!(shutdown_message, Message::Response(_)));
+    if let Message::Response(resp) = shutdown_message {
+        assert_eq!(resp.result, Some(serde_json::to_value(()).unwrap()));
+        assert!(resp.error.is_none());
+    }
+    client_receiver
+        .try_recv()
+        .expect_err("Unexpected message in the client channel");
 }
 
 #[test]
@@ -58,6 +85,21 @@ fn test_server_returns_successful_response_on_initialization() {
     if let Message::Response(response) = init_response {
         assert!(response.error.is_none());
     }
+    assert!(
+        client_conn.receiver.is_empty(),
+        "Unexpected message after initialization"
+    );
+}
+
+#[test]
+fn test_shutdown_handler_returns_response_to_the_client() {
+    setup_test_logging();
+    let (server_conn, client_conn) = Connection::memory();
+
+    send_shutdown_requests(&client_conn);
+    main_loop(&server_conn).unwrap();
+
+    assert_receiver_has_only_shutdown_response(client_conn.receiver);
 }
 
 #[test]
@@ -90,6 +132,10 @@ fn test_announce_text_document_sync_capabilities() {
             Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::Full))
         );
     }
+    client_conn
+        .receiver
+        .try_recv()
+        .expect_err("Unexpected message in client channel");
 }
 
 #[test]
@@ -98,31 +144,67 @@ fn test_server_publishes_diagnostic_after_receiving_didopen() {
     let (server_conn, client_conn) = Connection::memory();
 
     let document_url = Url::parse("file:///foo/bar").unwrap();
-    let opened_document = TextDocumentItem::new(
+    let text_document = TextDocumentItem::new(
         document_url.clone(),
         "move".to_string(),
         1,
         "mytext".to_string(),
     );
-    let didopen_notif = Notification::new(
-        "textDocument/didOpen".to_string(),
-        DidOpenTextDocumentParams {
-            text_document: opened_document,
+    let didopen_params = DidOpenTextDocumentParams { text_document };
+    let didopen_notif = notification_new::<DidOpenTextDocument>(didopen_params);
+    client_conn.sender.send(didopen_notif.into()).unwrap();
+
+    send_shutdown_requests(&client_conn);
+    main_loop(&server_conn).unwrap();
+
+    let diagnostics_message = client_conn.receiver.try_recv().unwrap();
+    assert_diagnostics(diagnostics_message, document_url, vec![]);
+
+    assert_receiver_has_only_shutdown_response(client_conn.receiver);
+}
+
+#[test]
+fn test_send_diagnostics_after_didchange() {
+    setup_test_logging();
+    let (server_conn, client_conn) = Connection::memory();
+
+    let document_url = Url::parse("file:///foo/bar").unwrap();
+    let text_document = VersionedTextDocumentIdentifier::new(document_url.clone(), 1);
+    let didchange_notif = Notification::new(
+        "textDocument/didChange".to_string(),
+        DidChangeTextDocumentParams {
+            text_document,
+            content_changes: vec![],
         },
     );
     client_conn
         .sender
-        .send(Message::Notification(didopen_notif))
+        .send(Message::Notification(didchange_notif))
         .unwrap();
 
     send_shutdown_requests(&client_conn);
     main_loop(&server_conn).unwrap();
 
     let diagnostics_message = client_conn.receiver.try_recv().unwrap();
-    assert!(matches!(diagnostics_message, Message::Notification(_)));
-    if let Message::Notification(notif) = diagnostics_message {
-        let params = notification_cast::<PublishDiagnostics>(notif).unwrap();
-        assert_eq!(params.uri, document_url);
-        assert_eq!(params.diagnostics, vec![]);
-    }
+    assert_diagnostics(diagnostics_message, document_url, vec![]);
+
+    assert_receiver_has_only_shutdown_response(client_conn.receiver);
+}
+
+#[test]
+fn test_send_nothing_after_didclose() {
+    setup_test_logging();
+    let (server_conn, client_conn) = Connection::memory();
+
+    let document_url = Url::parse("file:///foo/bar").unwrap();
+    let didclose_params = DidCloseTextDocumentParams {
+        text_document: TextDocumentIdentifier::new(document_url),
+    };
+    let didclose_notif = notification_new::<DidCloseTextDocument>(didclose_params);
+    client_conn.sender.send(didclose_notif.into()).unwrap();
+
+    send_shutdown_requests(&client_conn);
+    main_loop(&server_conn).unwrap();
+
+    assert_receiver_has_only_shutdown_response(client_conn.receiver);
 }
