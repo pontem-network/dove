1d44692117c630afd3ba9157135ea3394e69a9be
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2020-05-26 14:15:55 +0300

    normalize newlines for tests

--- crates/utils/src/io.rs
+++ crates/utils/src/io.rs
@@ -1,7 +1,7 @@
 use crate::{leaked_fpath, MoveFile, MoveFilePath};
 use anyhow::Result;
-use std::fs;
 use std::path::{Path, PathBuf};
+use std::{fs, io};
 
 fn iterate_directory<P: AsRef<Path>>(path: P) -> impl Iterator<Item = PathBuf> {
     walkdir::WalkDir::new(path)
@@ -33,26 +33,26 @@ pub fn iter_over_move_files<P: AsRef<Path>>(folder: P) -> Vec<String> {
 pub fn read_move_files<P: AsRef<Path>>(modules_folder: P) -> Vec<(MoveFilePath, String)> {
     let module_filenames = iter_over_move_files(modules_folder)
         .into_iter()
-        .map(leaked_fpath)
         .collect::<Vec<_>>();
 
     let mut lib_files = Vec::with_capacity(module_filenames.len());
     for mod_fname in module_filenames {
-        let mod_text = match fs::read_to_string(mod_fname) {
-            Ok(text) => text.replace("\r\n", "\n"),
-            Err(_) => {
-                log::warn!("Cannot read file {:?}, skipping", mod_fname);
+        let lib_file = match load_move_file(mod_fname.clone()) {
+            Ok(text) => text,
+            Err(io_error) => {
+                log::warn!("Cannot read file {:?} ({}) skipping", io_error, mod_fname);
                 continue;
             }
         };
-        lib_files.push((mod_fname, mod_text));
+        lib_files.push(lib_file);
     }
     lib_files
 }
 
-pub fn load_move_file<P: AsRef<Path>>(path: P) -> Result<MoveFile> {
+pub fn load_move_file<P: AsRef<Path>>(path: P) -> io::Result<MoveFile> {
     let fpath = leaked_fpath(path);
-    let text = fs::read_to_string(fpath)?;
+    let mut text = fs::read_to_string(fpath)?;
+    ra_vfs::normalize_newlines(&mut text);
     Ok((fpath, text))
 }
 
