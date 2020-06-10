26218fa606fa32f35aaa84eff19f2eba4ba9dc3a
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2020-06-10 17:28:35 +0300

    add build info to logging

--- crates/move-language-server/src/main.rs
+++ crates/move-language-server/src/main.rs
@@ -2,12 +2,21 @@ use anyhow::Result;
 
 use move_language_server::server;
 
+build_info::build_info!(fn crate_build_info);
+
 pub fn main() -> Result<()> {
     env_logger::init();
-    log::info!("starting language server");
+    log::info!("Starting language server");
+
+    let build_info = crate_build_info();
+    log::info!("Compiled at: {}", build_info.timestamp);
+    log::info!(
+        "Git: {:#?}",
+        build_info.version_control.as_ref().unwrap().git().unwrap()
+    );
 
     server::run_server()?;
 
-    log::info!("shutting down server");
+    log::info!("Shutting down server");
     Ok(())
 }
