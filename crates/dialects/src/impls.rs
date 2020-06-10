969c3df94d7ecce62aa22d33d011958b55ca3c65
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2020-06-10 14:55:17 +0300

    allow 0x sender for dfinance

--- crates/dialects/src/impls.rs
+++ crates/dialects/src/impls.rs
@@ -2,11 +2,12 @@ use anyhow::Result;
 
 use crate::base::Dialect;
 use crate::lang::gas::{dfinance_cost_table, libra_cost_table};
-use crate::shared::bech32::bech32_into_libra;
+use crate::shared::bech32::{bech32_into_libra, HRP};
 use crate::shared::errors::FileSourceMap;
 use crate::shared::ProvidedAccountAddress;
 use anyhow::Context;
 use libra_move_core_types::account_address::AccountAddress as LibraAccountAddress;
+use move_core_types::account_address::AccountAddress;
 use move_core_types::gas_schedule::CostTable;
 
 #[derive(Default)]
@@ -46,14 +47,19 @@ impl Dialect for DFinanceDialect {
     }
 
     fn normalize_account_address(&self, addr: &str) -> Result<ProvidedAccountAddress> {
-        let lowered_libra_address = bech32_into_libra(addr).with_context(|| {
-            format!("Address {:?} is not a valid wallet1 bech32 address", addr)
-        })?;
-        Ok(ProvidedAccountAddress::new(
-            addr.to_string(),
-            addr.to_string(),
-            lowered_libra_address,
-        ))
+        let address_res = if addr.starts_with(HRP) {
+            bech32_into_libra(addr).map(|lowered_addr| {
+                ProvidedAccountAddress::new(addr.to_string(), addr.to_string(), lowered_addr)
+            })
+        } else if addr.starts_with("0x") {
+            AccountAddress::from_hex_literal(addr).map(|address| {
+                let lowered_addr = format!("0x{}", address);
+                ProvidedAccountAddress::new(addr.to_string(), lowered_addr.clone(), lowered_addr)
+            })
+        } else {
+            Err(anyhow::anyhow!("Does not start with either wallet1 or 0x"))
+        };
+        address_res.with_context(|| format!("Address {:?} is not a valid dfinance address", addr))
     }
 
     fn cost_table(&self) -> CostTable {
