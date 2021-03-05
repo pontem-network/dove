1c3ee3a0127f13b9c21d42524265aff8eb31c650
Dmitry Yakushev <rig410@yandex.ru>
2021-03-05 12:52:48 +0300

    fixed loading dependencies from git (#9)
    * replace libra 1.0 with diem 1.1.4.
    
    * Fix for wasm build.
    
    * CI: update wasm-bindgen version
    
    * Update diem revision.
    
    * Fix for resource-viewer.
    
    * Added build all option.
    
    * Improve dove clean command.
    
    * CI: remove cached .cargo & cargo fetch
    
    Co-authored-by: Alexander Koz <a@koz.email>

--- dove/src/cmd/clean.rs
+++ dove/src/cmd/clean.rs
@@ -11,6 +11,12 @@ pub struct Clean {}
 impl Cmd for Clean {
     fn apply(self, ctx: Context) -> Result<(), Error> {
         let target = ctx.path_for(&ctx.manifest.layout.target);
+        let index_path = ctx.path_for(&ctx.manifest.layout.index);
+
+        if index_path.exists() {
+            fs::remove_file(index_path)?;
+        }
+
         if target.exists() {
             fs::remove_dir_all(target).map_err(Into::into)
         } else {
