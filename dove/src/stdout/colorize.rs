a8b4924e991cec550ced98a019a0b856c2f06f96
vladimirovmm <vladimirov.m.m@ya.ru>
2021-06-10 17:57:38 +0500

    cleaning code

--- dove/src/stdout/colorize.rs
+++ dove/src/stdout/colorize.rs
@@ -1,27 +1,22 @@
+use anyhow::{Result, Error};
 use std::io::Write;
 use std::sync::Mutex;
-use anyhow::Error;
 use once_cell::sync::OnceCell;
 use termcolor::{ColorSpec, Color, BufferWriter, WriteColor, ColorChoice};
-use crate::stdout::BufWrite;
 
 /// Marker to highlight; WHEN is 'always', 'never', or 'auto'
 static STDOUT_COLOR: OnceCell<Mutex<Box<ColorChoice>>> = OnceCell::new();
 
 /// get marker to highlight
-fn get_stdout_color() -> ColorChoice {
+fn get_colorchoise_color() -> ColorChoice {
     match STDOUT_COLOR.get() {
         Some(mt) => *(mt.lock().expect("Couldn't get STDOUT_COLOR")).clone(),
-        None => ColorChoice::Auto,
+        None => ColorChoice::Never,
     }
 }
 
-/// get flag --color=.. auto|always|never|ansi
-pub fn set_stdout_by_argv_flag_color<Out>(stdout: &Out) -> Result<(), Error>
-where
-    Out: BufWrite + Send,
-    Out: 'static,
-{
+/// set by flag --color=.. auto|always|never|ansi and stdout
+pub fn set_colorchoise_for_stdout() -> Result<(), Error> {
     let flag = std::env::args()
         .find_map(|it| {
             if it.to_lowercase().contains("--color=") {
@@ -36,7 +31,7 @@ where
         "always" => ColorChoice::Always,
         "ansi" => ColorChoice::AlwaysAnsi,
         "auto" => {
-            if stdout.is_stdout() && atty::is(atty::Stream::Stdout) {
+            if atty::is(atty::Stream::Stdout) {
                 ColorChoice::Auto
             } else {
                 ColorChoice::Never
@@ -44,21 +39,28 @@ where
         }
         _ => ColorChoice::Never,
     };
-    match STDOUT_COLOR.get() {
-        Some(mt) => {
-            let mut buff = mt.lock().expect("Couldn't get STDOUT_COLOR");
-            *buff = Box::new(color_flag);
-            Ok(())
-        }
-        None => STDOUT_COLOR
-            .set(Mutex::new(Box::new(color_flag)))
-            .map_or_else(|_| Err(anyhow!("Failed to set STDOUT_COLOR")), |_| Ok(())),
-    }
+    set_colorchoise(color_flag)
+}
+
+/// STDOUT_COLOR = ColorChoise::Never
+pub fn set_colorchoise_never() -> Result<(), Error> {
+    set_colorchoise(ColorChoice::Never)
+}
+
+/// set print to stdout
+fn set_colorchoise(color: ColorChoice) -> Result<(), Error> {
+    STDOUT_COLOR.set(Mutex::new(Box::new(color))).or_else(|mt| {
+        let mut bx = mt
+            .lock()
+            .map_err(|_| anyhow!("couldn't get access to STDOUT_STREAM"))?;
+        *bx = Box::new(color);
+        Ok(())
+    })
 }
 
 /// colorize text for output
 pub fn colorize_text(text: &str, spec: &mut ColorSpec) -> String {
-    let mut buffer = BufferWriter::stdout(get_stdout_color()).buffer();
+    let mut buffer = BufferWriter::stdout(get_colorchoise_color()).buffer();
     buffer
         .set_color(spec)
         .and(write!(&mut buffer, "{}", text))
