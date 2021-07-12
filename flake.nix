6520e24271a1519ccdbb030f478d292e9d080220
Vladimir Serov <me@cab404.ru>
2021-07-13 00:23:44 +0300

    small refactoring of flake.nix

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         llvmPackages = pkgs.llvmPackages_12;
 
         rustToolchain = fenixArch.latest;
-        rustToolchainWasm = rustTargets.wasm32-unknown-unknown.latest;
+        rustToolchainWasm = rustTargets.wasm32-unknown-unknown.stable;
 
         naersk-lib = naersk.lib.${system}.override {
           cargo = rustToolchain.toolchain;
@@ -28,43 +28,37 @@
       in {
 
         defaultPackage = naersk-lib.buildPackage (with pkgs; {
+
           src = ./.;
-          #src = ./dove;
-	  targets = [ "dove" ];
-	  buildInputs = [ pkg-config openssl ];
+	        targets = [ "dove" ];
+	        buildInputs = [ pkg-config openssl ];
+
           PROTOC = "${protobuf}/bin/protoc";
           PROTOC_INCLUDE="${protobuf}/include";
           LLVM_CONFIG_PATH="${llvmPackages.bintools}/bin/llvm-config";
-          LIBCLANG_PATH="${llvmPackages_11.clang-unwrapped.lib}/lib";
-          RUST_SRC_PATH = "${rustToolchain.rust-src}/lib/rustlib/src/rust/library/";
+          LIBCLANG_PATH="${llvmPackages.clang-unwrapped.lib}/lib";
 
-        #   SKIP_WASM_BUILD = 1;
         });
 
         defaultApp = utils.mkApp {
             drv = self.defaultPackage."${system}";
         };
 
-        devShell =
-        with pkgs; mkShell {
+        devShell = with pkgs; mkShell {
           buildInputs = [
-            protobuf openssl pre-commit pkg-config
-            llvmPackages.libclang.out
-            cargo-binutils
+            openssl pre-commit pkg-config
 
             (fenixArch.combine [
-              fenixArch.latest.toolchain
-              rustTargets.wasm32-unknown-unknown.stable.toolchain
-              # rustToolchainWasm.toolchain
+              rustToolchain.toolchain
+              rustToolchainWasm.toolchain
             ])
 
           ];
 
-#          SKIP_WASM_BUILD = 1;
           PROTOC = "${protobuf}/bin/protoc";
           PROTOC_INCLUDE="${protobuf}/include";
           LLVM_CONFIG_PATH="${llvmPackages.bintools}/bin/llvm-config";
-          LIBCLANG_PATH="${llvmPackages_11.clang-unwrapped.lib}/lib";
+          LIBCLANG_PATH="${llvmPackages.clang-unwrapped.lib}/lib";
           RUST_SRC_PATH = "${rustToolchain.rust-src}/lib/rustlib/src/rust/library/";
 
         };
