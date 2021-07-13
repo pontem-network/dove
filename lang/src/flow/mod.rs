f6b18ece5889556e99a9a0e9fe6a333fc7eb8b8a
Dm. Yakushev <rig410@yandex.ru>
2021-07-13 11:22:28 +0300

    Remove old builder.

--- lang/src/flow/mod.rs
+++ lang/src/flow/mod.rs
@@ -1,11 +1,10 @@
-use move_lang::parser::ast::Definition;
 use crate::compiler::file::MoveFile;
 use move_lang::errors::Error;
 use move_lang::{FullyCompiledProgram, parser};
+use move_lang::parser::ast::{Program, Definition};
 
 pub use crate::compiler::CompileFlow;
 
-pub mod builder;
 pub mod checker;
 
 pub trait DependencyResolver {
@@ -18,4 +17,30 @@ pub trait DependencyResolver {
         &mut self,
         ast: &parser::ast::Program,
     ) -> Result<Option<FullyCompiledProgram>, Error>;
+}
+
+pub struct StaticResolver {
+    deps: Vec<MoveFile<'static, 'static>>,
+}
+
+impl StaticResolver {
+    pub fn new(deps: Vec<MoveFile<'static, 'static>>) -> StaticResolver {
+        StaticResolver { deps }
+    }
+}
+
+impl DependencyResolver for StaticResolver {
+    fn resolve_source_deps(
+        &mut self,
+        _: &[Definition],
+    ) -> Result<Option<Vec<MoveFile<'static, 'static>>>, Error> {
+        Ok(Some(self.deps.clone()))
+    }
+
+    fn resolve_precompiled(
+        &mut self,
+        _: &Program,
+    ) -> Result<Option<FullyCompiledProgram>, Error> {
+        Ok(None)
+    }
 }
