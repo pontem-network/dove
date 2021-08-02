99ad417b3e6d412ede9c629735a7897a0a22ae97
Максим Владимиров <32911557+vladimirovmm@users.noreply.github.com>
2021-08-02 17:58:01 +0500

    bug fix for windows: git2 path too long (#91)

--- dove/src/manifest.rs
+++ dove/src/manifest.rs
@@ -1,6 +1,6 @@
 use std::{fmt, fs};
 use std::convert::TryFrom;
-use std::path::Path;
+use std::path::{Path, MAIN_SEPARATOR as MS};
 
 use anyhow::Error;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
@@ -82,23 +82,23 @@ fn tests_dir() -> String {
 }
 
 fn modules_output() -> String {
-    "artifacts/modules".to_owned()
+    format!("{}{}{}", artifacts(), MS, "modules")
 }
 
 fn scripts_output() -> String {
-    "artifacts/scripts".to_owned()
+    format!("{}{}{}", artifacts(), MS, "scripts")
 }
 
 fn transactions_output() -> String {
-    "artifacts/transactions".to_owned()
+    format!("{}{}{}", artifacts(), MS, "transactions")
 }
 
 fn bundles_output() -> String {
-    "artifacts/bundles".to_owned()
+    format!("{}{}{}", artifacts(), MS, "bundles")
 }
 
 fn move_prover_output() -> String {
-    "artifacts/move_prover".to_owned()
+    format!("{}{}{}", artifacts(), MS, "move_prover")
 }
 
 fn docs_output() -> String {
@@ -106,11 +106,11 @@ fn docs_output() -> String {
 }
 
 fn deps() -> String {
-    "artifacts/.external".to_owned()
+    format!("{}{}{}", artifacts(), MS, ".external")
 }
 
 fn chain_deps() -> String {
-    "artifacts/.external/chain".to_owned()
+    format!("{}{}{}", deps(), MS, "chain")
 }
 
 fn artifacts() -> String {
@@ -118,7 +118,7 @@ fn artifacts() -> String {
 }
 
 fn index() -> String {
-    "artifacts/.DoveIndex.toml".to_owned()
+    format!("{}{}{}", artifacts(), MS, ".DoveIndex.toml")
 }
 
 fn code_code_address() -> String {
