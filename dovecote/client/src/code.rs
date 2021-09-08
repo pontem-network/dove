f6f6f33f9ecbf71239711681ee74df8d71829e04
Dm. Yakushev <rig410@yandex.ru>
2021-09-08 10:37:04 +0300

    Remove unnecessary code.

--- dovecote/client/src/code.rs
+++ dovecote/client/src/code.rs
@@ -57,56 +57,9 @@ pub fn to_html(
         view_line.append_child(span.as_ref())?;
         lines.push(view_line);
     }
-
-    //<div style="top:324px;height:18px;" class="view-line">
-    // <span><span></span></span></div>
-    // <div style="top:342px;height:18px;" class="view-line">
-    // <span><span class="mtk5">use</span><span class="mtk1">&nbsp;</span><span class="mtk16">stdweb</span><span class="mtk1">::</span><span class="mtk16">web</span><span class="mtk1">::</span><span class="mtk16">event</span><span class="mtk1">::{</span></span></div><div style="top:360px;height:18px;" class="view-line"><span><span class="mtk1">&nbsp;&nbsp;&nbsp;&nbsp;</span><span class="mtk16">DoubleClickEvent</span><span class="mtk1">,</span></span></div><div style="top:378px;height:18px;" class="view-line"><span><span class="mtk1">&nbsp;&nbsp;&nbsp;&nbsp;</span><span class="mtk16">ClickEvent</span><span class="mtk1">,</span></span></div>
-
     Ok(lines)
 }
 
 pub fn to_code(lines: Vec<Element>) -> Result<String, JsValue> {
     todo!()
-}
-
-#[test]
-fn test() {
-    let code = r#"
-use wasm_bindgen::JsValue;
-use anyhow::{Error, anyhow};
-use wasm_bindgen::prelude::*;
-use web_sys::{Window, Document};
-
-pub fn api_url() -> String {
-    format!(
-        "{}/api/rpc",
-        web_sys::window()
-            .and_then(|win| win.location().origin().ok())
-            .unwrap_or_default()
-    )
-}
-
-pub fn window() -> Result<Window, Error> {
-    web_sys::window().ok_or_else(|| anyhow!("no global `window` exists"))
-}
-
-pub fn document(win: &Window) -> Result<Document, Error> {
-    win.document()
-        .ok_or_else(|| anyhow!("should have a document on window"))
-}
-    "#;
-
-    for line in code.lines() {
-        let mut first = true;
-        for i in line.split(' ') {
-            if first {
-                first = false;
-                print!("{}", i);
-            } else {
-                print!(" {}", i);
-            }
-        }
-        println!();
-    }
 }
