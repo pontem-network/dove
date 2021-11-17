1acd9227cdbb430d0453523e681504993c83d07c
Максим Владимиров <32911557+vladimirovmm@users.noreply.github.com>
2021-11-17 17:39:52 +0500

    $ dove clean [ALL|STATE] (#163)

--- dove/tests/test_cmd_dove_build.rs
+++ dove/tests/test_cmd_dove_build.rs
@@ -1,20 +1,15 @@
 mod helper;
 
-use std::path::PathBuf;
 use std::fs;
-use std::io::{Write, Read, BufRead, BufReader};
-use std::collections::HashMap;
-use std::fs::read_to_string;
-use anyhow::Result;
-use dialect::Dialect;
-use crate::helper::{create_new_project, delete_project, execute_dove_at};
+use std::io::Read;
+use crate::helper::{delete_project, execute_dove_at, new_demo_project};
 
 /// Build a project without additional parameters
 /// $ dove build
 #[test]
 fn test_cmd_dove_build_without_arguments() {
     let project_name = "project_build_without_arguments";
-    let project_path = create_project(&project_name).unwrap();
+    let project_path = new_demo_project(&project_name).unwrap();
 
     execute_dove_at(&["build"], &project_path).unwrap();
 
@@ -26,7 +21,7 @@ fn test_cmd_dove_build_without_arguments() {
 #[test]
 fn test_cmd_dove_build_with_doc() {
     let project_name = "project_build_build_with_doc";
-    let project_path = create_project(&project_name).unwrap();
+    let project_path = new_demo_project(&project_name).unwrap();
 
     execute_dove_at(&["build", "-d"], &project_path).unwrap();
 
@@ -44,7 +39,7 @@ fn test_cmd_dove_build_with_doc() {
 #[test]
 fn test_cmd_dove_build_with_package() {
     let project_name = "project_build_build_with_package";
-    let project_path = create_project(&project_name).unwrap();
+    let project_path = new_demo_project(&project_name).unwrap();
 
     execute_dove_at(
         &[
@@ -85,34 +80,12 @@ fn test_cmd_dove_build_with_package() {
 #[test]
 fn test_cmd_dove_build_error_map() {
     let project_name = "project_build_build_error_map";
-    let project_path = create_project(&project_name).unwrap();
+    let project_path = new_demo_project(&project_name).unwrap();
 
     execute_dove_at(&["build", "--error-map", "error_map"], &project_path).unwrap();
     assert!(project_path.join("error_map.errmap").exists());
 
     delete_project(&project_path).unwrap();
-}
-
-fn create_project(project_name: &str) -> Result<PathBuf> {
-    let t = [("Demo", "0x2")].into_iter().collect();
-    let project_path = create_new_project(&project_name, t)?;
-    // scripts/main.move
-    let mut main_script = fs::File::create(project_path.join("scripts").join("main.move"))?;
-    main_script.write(b"script { fun main(){} }");
-
-    // sources/demo1v.move
-    let mut demo1v_script = fs::File::create(project_path.join("sources").join("demo1v.move"))?;
-    demo1v_script.write(b"module Demo::Demo1v{ fun run(){ } }");
-
-    // sources/demo2v.move
-    let mut demo2v_script = fs::File::create(project_path.join("sources").join("demo2v.move"))?;
-    demo2v_script.write(b"module Demo::Demo2v{ fun run(){ } }");
-
-    // sources/demo3v.move
-    let mut demo3v_script = fs::File::create(project_path.join("sources").join("demo3v.move"))?;
-    demo3v_script.write(b"module Demo::Demo3v{ fun run(){ } }");
-
-    Ok(project_path)
 }
 
 fn find_u8(source: &[u8], need: &[u8]) -> bool {
