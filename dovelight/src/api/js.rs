ec297f00e28af350e966ed1f81bd897e6b2fcac4
vladimirovmm <vladimirov.m.m@ya.ru>
2021-11-23 17:15:05 +0500

    Request preparation: rpc resource

--- dovelight/src/api/js.rs
+++ dovelight/src/api/js.rs
@@ -1,7 +1,9 @@
 use std::fmt::Display;
+use anyhow::Result;
 use wasm_bindgen::prelude::*;
 use crate::lang::compiler::source_map::SourceMap;
 use crate::api;
+use crate::loader::get_request_resource_by_xpath;
 
 #[wasm_bindgen]
 pub fn build(
@@ -54,6 +56,21 @@ pub fn tx(
     let source_map: SourceMap = source_map.into_serde().map_err(js_err)?;
     let unit = api::tx(chain_api, source_map, dialect, call).map_err(js_err)?;
     JsValue::from_serde(&unit).map_err(js_err)
+}
+
+/// Creating a transaction
+#[wasm_bindgen]
+pub fn request_resource_json(
+    // account address
+    // example: 0x1
+    account: String,
+    // xpath
+    // example: 0x1::Account::Balance<0x1::Coins::ETH>
+    xpath: String,
+) -> Result<JsValue, JsValue> {
+    get_request_resource_by_xpath(&account, &xpath)
+        .map_err(js_err)
+        .and_then(|value| JsValue::from_serde(&value).map_err(js_err))
 }
 
 pub fn js_err<D: Display>(err: D) -> JsValue {
--- dovelight/src/loader.rs
+++ dovelight/src/loader.rs
@@ -1,8 +1,12 @@
-use crate::lang::deps::DependencyLoader;
-use move_core_types::language_storage::ModuleId;
-use anyhow::{Error, anyhow};
-use serde::{Serialize, Deserialize};
 use std::fmt::{Display, Formatter};
+use serde::{Serialize, Deserialize};
+use serde_json::Value;
+use anyhow::{Error, anyhow, bail};
+use move_core_types::language_storage::{ModuleId, StructTag, TypeTag};
+use move_core_types::account_address::AccountAddress;
+use lang::compiler::address::ss58::address_to_ss58;
+use lang::tx::parser::parse_tp_param;
+use crate::lang::deps::DependencyLoader;
 use crate::env::http::http_request;
 use crate::env::http::Request as HttpRequest;
 
@@ -84,4 +88,46 @@ impl Display for ErrorMsg {
             write!(f, "Code:{}", self.code.unwrap_or_default())
         }
     }
+}
+
+/// Generate a JSON request to get a resource
+pub fn get_request_resource_by_xpath(account: &str, xpath: &str) -> Result<Value, Error> {
+    let account = AccountAddress::from_hex_literal(account)?;
+    let tag = pars_xpath(xpath)?;
+    let req = Request {
+        id: 1,
+        jsonrpc: "2.0",
+        method: "mvm_getResource",
+        params: vec![
+            address_to_ss58(&account),
+            format!("0x{}", hex::encode(bcs::to_bytes(&tag)?)),
+        ],
+    };
+
+    Ok(serde_json::to_value(&req)?)
+}
+
+fn pars_xpath(xpath: &str) -> Result<StructTag, Error> {
+    match parse_tp_param(xpath)? {
+        TypeTag::Struct(tag) => Ok(tag),
+        _ => bail!("The parameter is set incorrectly {xpath:?}. Expected: ADDRESS::MODEL_NAME::RESOURCE_NAME<TYPE_1, TYPE_2...TYPE_N>", xpath = xpath),
+    }
+}
+
+#[test]
+fn test_get_request_resource_by_xpath() {
+    let mut tag =
+        get_request_resource_by_xpath("0x1", "0x1::Account::Balance<0x1::Coins::ETH>").unwrap();
+
+    assert_eq!(
+        r#"{"id":1,"jsonrpc":"2.0","method":"mvm_getResource","params":["5C4hrfjw9DjXZTzV3MwzrrAr9P1MJhSrvWGWqi1eSuyUqAsg","0x0000000000000000000000000000000000000000000000000000000000000001074163636f756e740742616c616e63650107000000000000000000000000000000000000000000000000000000000000000105436f696e730345544800"]}"#,
+        &tag
+    );
+
+    tag = get_request_resource_by_xpath("0x1", "0x1::Account::Balance").unwrap();
+
+    assert_eq!(
+        r#"{"id":1,"jsonrpc":"2.0","method":"mvm_getResource","params":["5C4hrfjw9DjXZTzV3MwzrrAr9P1MJhSrvWGWqi1eSuyUqAsg","0x0000000000000000000000000000000000000000000000000000000000000001074163636f756e740742616c616e636500"]}"#,
+        &tag
+    );
 }
