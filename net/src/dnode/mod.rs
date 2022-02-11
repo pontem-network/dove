d3cbc1d5586c4025cf39dcb7769c70f14111c36e
vladimirovmm <vladimirov.m.m@ya.ru>
2022-02-11 21:21:25 +0500

    non-working code is commented out

--- net/src/dnode/mod.rs
+++ net/src/dnode/mod.rs
@@ -1,9 +1,8 @@
 #![cfg(feature = "dfinance")]
 
 mod client;
-
-use http::Uri;
 use anyhow::Result;
+use url::Url;
 
 use move_core_types::language_storage::{ModuleId, StructTag};
 use move_core_types::account_address::AccountAddress;
@@ -12,7 +11,7 @@ use crate::{Net, Block, BytesForBlock};
 use crate::dnode::client::data_request;
 
 pub struct DnodeNet {
-    pub(crate) uri: Uri,
+    pub(crate) uri: Url,
 }
 
 impl Net for DnodeNet {
@@ -21,12 +20,17 @@ impl Net for DnodeNet {
         module_id: &ModuleId,
         height: &Option<Block>,
     ) -> Result<Option<BytesForBlock>> {
-        let address = self.dialect.adapt_address_to_target(*module_id.address());
+        // does not work without dialect
+        // let address = self.dialect.adapt_address_to_target(*module_id.address());
+        // remove this
+        let address = module_id.address().into_bytes();
+
         let bytes = data_request(&address, &module_id.access_vector(), &self.uri, height).ok();
         match bytes {
             None => Ok(None),
-            Some(mut bytes) => {
-                self.dialect.adapt_to_basis(&mut bytes.0)?;
+            Some(bytes) => {
+                // does not work without dialect
+                // self.dialect.adapt_to_basis(&mut bytes.0)?;
                 Ok(Some(BytesForBlock(bytes.0, bytes.1)))
             }
         }
@@ -38,13 +42,18 @@ impl Net for DnodeNet {
         tag: &StructTag,
         height: &Option<Block>,
     ) -> Result<Option<BytesForBlock>> {
-        let address = self.dialect.adapt_address_to_target(*address);
+        // does not work without dialect
+        // let address = self.dialect.adapt_address_to_target(*address);
+        // remove this
+        let address = address.into_bytes();
+
         let access_vector = tag.access_vector();
         let bytes = data_request(&address, &access_vector, &self.uri, height).ok();
         match bytes {
             None => Ok(None),
-            Some(mut bytes) => {
-                self.dialect.adapt_to_basis(&mut bytes.0)?;
+            Some(bytes) => {
+                // does not work without dialect
+                // self.dialect.adapt_to_basis(&mut bytes.0)?;
                 Ok(Some(BytesForBlock(bytes.0, bytes.1)))
             }
         }
