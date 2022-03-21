f0548dbf385ca70379d773c432139f81fc89b32b
Максим Владимиров <32911557+vladimirovmm@users.noreply.github.com>
2022-03-21 14:18:08 +0500

    fix: deploy/call doesn’t work with remote server - doesn’t return result sometimes  (#225)
    * fix: pontemapi
    
    * Change to Output
    
    * rename val address => hash
    
    * pontemapi: updated version

--- dove/src/cmd/call.rs
+++ dove/src/cmd/call.rs
@@ -50,8 +50,8 @@ impl ExecuteTransaction {
 
         Publish::try_from((&self.request, path_transaction))?
             .apply()
-            .map(|address| {
-                println!("Address: {}", address);
+            .map(|hash| {
+                println!("Hash: {hash}");
             })
     }
 }
